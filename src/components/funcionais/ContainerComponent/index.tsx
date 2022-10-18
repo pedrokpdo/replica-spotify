d91d160b8dfac8cbab25a80023fc121395a04ba4
pedrokpdo <pedromotoehm@gmail.com>
2022-10-18 16:41:25 -0300

    Header (MusicListComponent)

--- src/components/funcionais/ContainerComponent/index.tsx
+++ src/components/funcionais/ContainerComponent/index.tsx
@@ -1,9 +1,21 @@
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { ScrollView, View } from 'react-native'
+import { Space } from "../SpaceComponent";
 
-export const ContainerComponent = ({ children }) => {
+interface Props {
+    children?: any;
+    back?: any;
+}
+
+export const ContainerComponent = ({ children, back }: Props) => {
     return (
         <ScrollView style={{ flex: 1, backgroundColor: 'rgb(19, 18, 18)' }}>
+            {back ? (
+                <Space top={1}>
+                    <Ionicons name="arrow-back" size={24} color={'white'} />
+                </Space>
+            ) : null}
             {children}
         </ScrollView>
     )
--- src/screens/MusicListScreen/index.tsx
+++ src/screens/MusicListScreen/index.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { Image, View } from "react-native";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
+//@ts-ignore
+import lual from '../../assets/lual.png'
+import { Space } from "../../components/funcionais/SpaceComponent";
+import { Text } from "../../components/funcionais/TextComponent";
+import { Row } from "../../components/funcionais/RowComponent";
+import { AntDesign, Entypo, FontAwesome, MaterialCommunityIcons, SimpleLineIcons } from "@expo/vector-icons";
+import { Title } from "../../components/funcionais/TitleComponent";
 
 export const MusicListScreen = () => {
     return (
-        <ContainerComponent>
+        <ContainerComponent back>
+            <Space>
+                <Image style={{ width: 160, height: 160, alignSelf: 'center' }} source={lual} />
+                <Text top={1} style={{ color: 'gray' }}>Versões desplugadas para curtir um reggae com os pés na areia!</Text>
+                <Row top={1} style={{ alignItems: 'center' }}>
+                    <FontAwesome name="spotify" size={32} color='green' />
+                    <Title style={{ marginLeft: 8 }}>Spotify</Title>
+                </Row>
+                <Title top={1} style={{ color: 'gray', fontSize: 16 }}>72.350 curtidas . 4h 44min</Title>
+                <Row top={2} style={{ justifyContent: 'space-between' }}>
+                    <Row style={{ width: 180, justifyContent: 'space-around' }}>
+                        <AntDesign name="hearto" color={'gray'} size={24} />
+                        <AntDesign name="download" color={'gray'} size={24} />
+                        <MaterialCommunityIcons name="microphone-plus" color={'gray'} size={24} />
+                        <SimpleLineIcons name="options-vertical" color={'gray'} size={24} />
+                    </Row>
+                    <Row style={{ alignItems: 'center' }}>
+                        <MaterialCommunityIcons name="repeat" size={28} color={'green'} />
+                        <View style={{ marginLeft: 16, alignItems: 'center', backgroundColor: 'green', borderRadius: 222, justifyContent: 'center', width: 40, height: 40 }}>
+                            <Entypo name="controller-play" color={'black'} size={24} />
+                        </View>
+                    </Row>
+                </Row>
+            </Space>
 
         </ContainerComponent>
     )
