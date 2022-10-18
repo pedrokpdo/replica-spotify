52096f6f13465f347527ff021ffacaa534cee9d8
pedrokpdo <pedromotoehm@gmail.com>
2022-10-18 17:26:42 -0300

    Preparando Stacks

--- src/stacks/AppStack/index.tsx
+++ src/stacks/AppStack/index.tsx
@@ -1,13 +1,17 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import { HomeStack } from '../HomeStack'
+import { LibaryStack } from '../LibaryStack'
+import { SearchStack } from '../SearchStack'
 
 const { Screen, Navigator } = createBottomTabNavigator()
 
 export const AppStack = () => {
     return (
         <Navigator screenOptions={{ headerShown: false }}>
-            <Screen name='HomeStack' component={HomeStack}/>
+            <Screen name='HomeStack' component={HomeStack} />
+            <Screen name='SearchStack' component={SearchStack} />
+            <Screen name='LibaryStack' component={LibaryStack} />
         </Navigator>
     )
 }
---
+++ src/stacks/SearchStack/index.tsx
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import { HomeScreen } from '../../screens/HomeScreen'
+import { MusicListScreen } from '../../screens/MusicListScreen'
+import { MusicScreen } from '../../screens/MusicScreen'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export const SearchStack = () => {
+    return (
+        <Navigator screenOptions={{ headerShown: false }}>
+            <Screen name='HomeScreen' component={HomeScreen} />
+            <Screen name='MusicListScreen' component={MusicListScreen} />
+            <Screen name='MusicScreen' component={MusicScreen} />
+        </Navigator>
+    )
+}
