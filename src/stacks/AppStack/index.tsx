c222a4f7444220d90875fcd309cd4f79b835d86e
pedrokpdo <pedromotoehm@gmail.com>
2022-10-19 13:25:28 -0300

    ajustando stack

--- src/stacks/AppStack/index.tsx
+++ src/stacks/AppStack/index.tsx
@@ -9,10 +9,10 @@ const { Screen, Navigator } = createBottomTabNavigator()
 
 export const AppStack = () => {
     return (
-        <Navigator screenOptions={{ tabBarStyle: {backgroundColor: 'black'}, headerShown: false }} defaultScreenOptions={{ tabBarActiveBackgroundColor: 'black', tabBarInactiveBackgroundColor: 'black' }}>
-            <Screen name='HomeStack' component={HomeStack} options={{ tabBarIcon: () => (<Ionicons  name='home' size={24} color='white' />) }} />
-            <Screen name='SearchStack' component={SearchStack} options={{ tabBarIcon: () => (<Ionicons name='search' size={24} color='white' />) }} />
-            <Screen name='LibaryStack' component={LibaryStack} options={{ tabBarIcon: () => (<Ionicons name='library' size={24} color='white' />) }} />
+        <Navigator screenOptions={{ tabBarStyle: { backgroundColor: 'black' }, headerShown: false }} >
+            <Screen name='HomeStack' component={HomeStack} options={{ tabBarIcon: () => (<Ionicons name='home' size={24} color='white' />) , tabBarActiveTintColor: 'white', tabBarInactiveTintColor: 'white'}} />
+            <Screen name='SearchStack' component={SearchStack} options={{ tabBarIcon: () => (<Ionicons name='search' size={24} color='white' />) , tabBarActiveTintColor: 'white', tabBarInactiveTintColor: 'white'}} />
+            <Screen name='LibaryStack' component={LibaryStack} options={{ tabBarIcon: () => (<Ionicons name='library' size={24} color='white' />) , tabBarActiveTintColor: 'white', tabBarInactiveTintColor: 'white'}} />
         </Navigator>
     )
 }
