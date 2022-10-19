7eb6ef1c599761637affde620ad79edcfffd6b64
pedrokpdo <pedromotoehm@gmail.com>
2022-10-19 11:53:41 -0300

    Rotas

---
+++ src/screens/HistoryListScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
+
+export const HistoryListScreen = () => {
+    return (
+        <ContainerComponent>
+            
+        </ContainerComponent>
+    )
+}
--- src/stacks/HomeStack/index.tsx
+++ src/stacks/HomeStack/index.tsx
@@ -1,8 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
+import { HistoryListScreen } from '../../screens/HistoryListScreen'
 import { HomeScreen } from '../../screens/HomeScreen'
 import { MusicListScreen } from '../../screens/MusicListScreen'
 import { MusicScreen } from '../../screens/MusicScreen'
+import { NotificationListScreen } from '../../screens/NotificationListScreen'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
@@ -12,6 +14,8 @@ export const HomeStack = () => {
             <Screen name='HomeScreen' component={HomeScreen} />
             <Screen name='MusicListScreen' component={MusicListScreen} />
             <Screen name='MusicScreen' component={MusicScreen} />
+            <Screen name='NotificationListScreen' component={NotificationListScreen} />
+            <Screen name='HistoryListScreen' component={HistoryListScreen} />
         </Navigator>
     )
 }
